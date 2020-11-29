c1c90fcdd9f8231b5a04ed8b323fe3791cc81083
Mauricio G <mauriciogarzonb@gmail.com>
2020-11-29 14:34:17 -0500

    Server configuration

--- assets/config.js
+++ assets/config.js
@@ -1,13 +1,13 @@
 let config = {
   api: {
     // Local
-    url: 'http://localhost:3001/api/v1/',
+    url: 'http://192.168.0.15:3001/api/v1/',
     src_movie_cover: 'https://image.tmdb.org/t/p/w500',
     src_movie_backdrop: 'https://image.tmdb.org/t/p/original',
     src_game: 'https://images.igdb.com/igdb/image/upload/t_720p/',
     src_game2: 'https://images.igdb.com/igdb/image/upload/t_1080p/',
-    movies_path: 'D:\\utorrent\\movies\\',
-    games_path: 'D:\\utorrent\\games\\'
+    movies_path: 'C:\\hcc\\content\\Movies\\',
+    games_path: 'C:\\hcc\\content\\Games\\'
   },
   routes: [
     {
--- nuxt.config.js
+++ nuxt.config.js
@@ -77,4 +77,7 @@ export default {
    ** See https://nuxtjs.org/api/configuration-build/
    */
   build: {},
+  server: {
+    host: "0.0.0.0"
+  }
 }
